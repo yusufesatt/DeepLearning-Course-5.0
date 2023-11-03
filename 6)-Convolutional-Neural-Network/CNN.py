2b20c496cd9187bccff69f20c8dcf0525c48ceae
Yusuf Esat <xyusufesat@gmail.com>
2023-11-03 22:53:19 +0300

    Initialize CNN and Dataset links added

---
+++ 6)-Convolutional-Neural-Network/CNN.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  3 11:06:43 2023
+
+@author: yusufesat
+"""
+
+# %%
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+# %%
+
+train = pd.read_csv ("C:/Users/yusufesat/Desktop/Deep-Learning-5.0/6)-Convolutional-Neural-Network/Mnist-Data/train.csv")
+print(train.shape)
+train.head
+
+# %%
+
+test = pd.read_csv("C:/Users/yusufesat/Desktop/Deep-Learning-5.0/6)-Convolutional-Neural-Network/Mnist-Data/test.csv")
+print(test.shape)
+test.head
+
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
